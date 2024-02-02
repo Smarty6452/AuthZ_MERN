14cf04077ddef94b3d3280b61c2beb95f1ac28e3
Rohit Bharti <71224966+Smarty6452@users.noreply.github.com>
2024-02-03 00:01:00 +0530

    connected mongo-db

--- backend/api/index.js
+++ backend/api/index.js
@@ -1,4 +1,19 @@
 import express from "express"
+import mongoose from "mongoose"
+import dotenv from "dotenv"
+dotenv.config()
+
+mongoose.connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }).then(() => {
+    console.log("db connected succesfully")
+  }).catch((err) => {
+    console.log("db not conneccted" + err)
+  })
+
+ 
+
 
 const app = express()
 
