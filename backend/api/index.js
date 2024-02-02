333fd174ab0a27b3e418e64d9070599401c45a72
Rohit Bharti <71224966+Smarty6452@users.noreply.github.com>
2024-02-03 00:43:07 +0530

    create api routes

--- backend/api/index.js
+++ backend/api/index.js
@@ -1,22 +1,25 @@
-import express from "express"
-import mongoose from "mongoose"
-import dotenv from "dotenv"
-dotenv.config()
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+import userRoutes from "./routes/user.route.js";
 
-mongoose.connect(process.env.MONGODB_URI, {
+mongoose
+  .connect(process.env.MONGODB_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-  }).then(() => {
-    console.log("db connected succesfully")
-  }).catch((err) => {
-    console.log("db not conneccted" + err)
   })
+  .then(() => {
+    console.log("db connected succesfully");
+  })
+  .catch((err) => {
+    console.log("db not conneccted" + err);
+  });
 
- 
-
-
-const app = express()
+const app = express();
 
 app.listen(3000, () => {
-    console.log(`server listening on 3000 `)
-})
+  console.log(`server listening on 3000 `);
+});
+
+app.use("/api/user", userRoutes);
