31df31f9ae691a3ac96a9d9b6ae05b4b93040a9d
Rohit Bharti <71224966+Smarty6452@users.noreply.github.com>
2024-02-03 22:06:51 +0530

    add a midlleware to handle error

--- backend/api/controllers/auth.controller.js
+++ backend/api/controllers/auth.controller.js
@@ -1,7 +1,7 @@
 import User from "../models/user.model.js";
 import bcrypt from "bcryptjs"
 
-export const signup = async (req, res) => {
+export const signup = async (req, res, next) => {
   // console.log(req.body);
   const { username, email, password } = req.body;
   const hashedpassword = bcrypt.hashSync(password, 10);
@@ -12,7 +12,7 @@ export const signup = async (req, res) => {
 
   res.status(201).json({ message: "User created succesfully"})
   } catch (error) {
-    res.status(500).json(error.message)
+    next(error)
     
   }
 
---
+++ backend/api/utils/error.js
@@ -0,0 +1,10 @@
+import e from "express";
+
+export const errorHandler = (statusCode, message) => {
+    const error = new Error(message);
+    error.statusCode = statusCode
+    error.message = message;
+    return error;
+}
+
+export default errorHandler
