abbbc5698ad5960a27dff146e4cce235f77b09ce
Rohit Bharti <71224966+Smarty6452@users.noreply.github.com>
2024-02-04 16:13:49 +0530

    ccomplete signin page functionality

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,9 +1,88 @@
-import React from 'react'
+import React, { useState } from "react";
+import axios from "axios"
+import {Link, useNavigate} from "react-router-dom"
 
 const SignIn = () => {
+  const [formData, setFormData] = useState({});
+  const [error , setError] = useState(false)
+  const [loading , setLoading] = useState(false)
+  const navigate = useNavigate()
+
+  const handleChange = (e) => {
+    e.preventDefault();
+    setFormData({ ...formData, [e.target.id]: e.target.value });
+  };
+  // console.log(formData);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      setLoading(true);
+      const response = await axios.post('/api/auth/signin', formData, {
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+
+      // Log the entire response for debugging
+      console.log(response);
+
+      const data = response.data;
+      setLoading(false);
+      setError(false);
+
+      if (data.success === false) {
+        setError(true);
+        return;
+      }
+
+      navigate("/")
+
+      console.log(JSON.stringify(data));
+    } catch (error) {
+      console.error("Error during API request:", error);
+      setError(true);
+      setLoading(false);
+    }
+  };
+  
+  
+
+
   return (
-    <div>SignIn</div>
-  )
-}
+    <div className="p-3 max-w-lg mx-auto">
+      <h1 className="text-3xl text-center font-semibold my-7">Sign In</h1>
+
+      <form onSubmit={handleSubmit} className="flex flex-col  gap-4">
+       
+        <input
+          type="text"
+          placeholder="email"
+          id="email"
+          onChange={handleChange}
+          className="bg-slate-100 p-3 rounded-lg"
+        />
+        <input
+          type="text"
+          placeholder="password"
+          id="password"
+          onChange={handleChange}
+          className="bg-slate-100 p-3 rounded-lg"
+        />
+
+        <button disabled={loading} className="bg-slate-700 text-white p-4 rounded-lg uppercase hover:opacity-95 disabled:opacity-80">
+          {" "}
+          {loading? 'loading...' : "Sign In"}
+        </button>
+      </form>
+      <div className="flex py-2 text-sm">
+        <p className="text-gray-500">Dont Have an account ?</p>
+        <Link to="/signup">
+        <span className="text-blue-500 px-2">Sign up</span></Link>
+      </div>
+      <p className='text-red-700 mt-5'>{error && 'Something went wrong!'}</p>
+    </div>
+  );
+};
 
 export default SignIn
--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,10 +1,14 @@
 import React, { useState } from "react";
-import axios from "axios"
+import axios from "axios";
+import { Link, useNavigate } from "react-router-dom";
 
 const SignUp = () => {
   const [formData, setFormData] = useState({});
-  const [error , setError] = useState(false)
-  const [loading , setLoading] = useState(false)
+  const [error, setError] = useState(false);
+  const [loading, setLoading] = useState(false);
+  ate(false);
+  const navigate = useNavigate();
+
   const handleChange = (e) => {
     e.preventDefault();
     setFormData({ ...formData, [e.target.id]: e.target.value });
@@ -15,28 +19,31 @@ const SignUp = () => {
     e.preventDefault();
     try {
       setLoading(true);
-      const response = await axios.post('/api/auth/signup', formData, {
+      const response = await axios.post("/api/auth/signup", formData, {
         headers: {
-          'Content-Type': 'application/json',
+          "Content-Type": "application/json",
         },
       });
 
-      const data = await response.json
-      setLoading(false)
-      setError(false)
+      // Log the entire response for debugging
+      console.log(response);
+
+      const data = response.data;
+      setLoading(false);
+      setError(false);
+
       if (data.success === false) {
         setError(true);
         return;
       }
-      console.log(data + "user Created Succesfully");
+      navigate("/signin");
+      console.log(JSON.stringify(data));
     } catch (error) {
       console.error("Error during API request:", error);
-      setError(true)
-      setLoading(false)
+      setError(true);
+      setLoading(false);
     }
   };
-  
-
 
   return (
     <div className="p-3 max-w-lg mx-auto">
@@ -48,7 +55,6 @@ const SignUp = () => {
           placeholder="username"
           id="username"
           onChange={handleChange}
-         
           className="bg-slate-100 p-3 rounded-lg"
         />
         <input
@@ -66,18 +72,23 @@ const SignUp = () => {
           className="bg-slate-100 p-3 rounded-lg"
         />
 
-        <button disabled={loading} className="bg-slate-700 text-white p-4 rounded-lg uppercase hover:opacity-95 disabled:opacity-80">
+        <button
+          disabled={loading}
+          className="bg-slate-700 text-white p-4 rounded-lg uppercase hover:opacity-95 disabled:opacity-80"
+        >
           {" "}
-          {loading? 'loading...' : "Sign Up"}
+          {loading ? "loading..." : "Sign Up"}
         </button>
       </form>
       <div className="flex py-2 text-sm">
         <p className="text-gray-500">Have an account ?</p>
-        <span className="text-blue-500 px-2">Sign in</span>
+        <Link to="/signin">
+          <span className="text-blue-500 px-2">Sign in</span>
+        </Link>
       </div>
-      <p className='text-red-700 mt-5'>{error && 'Something went wrong!'}</p>
+      <p className="text-red-700 mt-5">{error && "Something went wrong!"}</p>
     </div>
   );
 };
 
-export default SignUp
+export default SignUp;
